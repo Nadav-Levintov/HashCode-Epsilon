eeb86162ffcdc9f3aae13ff6d18bb639d7e4570a
Nadav-Levintov <DSP155@Gmail.com>
2017-02-23 20:48:33 +0200

    Parser + data types in H

--- HashCode/HashCode/parse_input.h
+++ HashCode/HashCode/parse_input.h
@@ -6,4 +6,25 @@
 #include <fstream>
 #include <iomanip>
 
+class Data
+{
+public:
+	int v, e, r, c, x;
+	int *video_sizes;
+	Endpoint_Data* endpoint_data_arr;
+
+	~Data()
+	{
+		delete[] video_sizes;
+	}
+};
+
+class Endpoint_Data
+{
+public:
+	int data_center_latency;
+	int num_of_caches;
+	int *caches_latency;
+};
+
 #endif // !PARSE_INPUT

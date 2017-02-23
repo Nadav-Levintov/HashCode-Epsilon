b37ad5fab364daea6ca9f92d5487c519911ddffa
Nadav-Levintov <DSP155@Gmail.com>
2017-02-23 20:48:47 +0200

    Parser+ types in H

--- HashCode/HashCode/parse_input.cpp
+++ HashCode/HashCode/parse_input.cpp
@@ -1,26 +1,7 @@
 #include "parse_input.h"
 using namespace std;
 
-class Data
-{
-public:
-	int v, e, r, c, x;
-	int *video_sizes;
-	Endpoint_Data* endpoint_data_arr;
 
-	~Data()
-	{
-		delete[] video_sizes;
-	}
-};
-
-class Endpoint_Data
-{
-public:
-	int data_center_latency;
-	int num_of_caches;
-	int *caches_latency;
-};
 
 void parser(Data* ourData)
 {

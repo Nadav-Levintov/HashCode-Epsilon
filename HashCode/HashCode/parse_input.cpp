e8245692567a3356f2907247a1742c559802a88e
Nadav-Levintov <DSP155@Gmail.com>
2017-02-23 21:55:52 +0200

    Com1

--- HashCode/HashCode/parse_input.cpp
+++ HashCode/HashCode/parse_input.cpp
@@ -18,23 +18,30 @@ void parser(Data* ourData)
 	ourData->endpoint_data_arr = new Endpoint_Data[ourData->e];
 
 	string video_stize_temp;
-	ourData->video_sizes = new int[ourData->v];
+	ourData->video_arr = new Video[ourData->v];
 
 	int i = 0, j = 0;
-	for (i = 0; i < ourData->v; i++);
+	for (i = 0; i < ourData->v; i++)
 	{
 		cin >> video_stize_temp;
-		ourData->video_sizes[i] = stoi(video_stize_temp);
+		ourData->video_arr[i].size = stoi(video_stize_temp);
 	}
 
-	string latancy_temp, num_of_caches_temp,cache_id_temp,cache_latancy_temp;
-	for (i = 0; i < ourData->e; i++);
+	string latancy_temp, num_of_caches_temp, cache_id_temp, cache_latancy_temp;
+	for (i = 0; i < ourData->e; i++)
 	{
 		cin >> latancy_temp;
 		ourData->endpoint_data_arr[i].data_center_latency = stoi(latancy_temp);
 		cin >> num_of_caches_temp;
 		ourData->endpoint_data_arr[i].num_of_caches = stoi(num_of_caches_temp);
-		ourData->endpoint_data_arr[i].caches_latency = new int[ourData->endpoint_data_arr[i].num_of_caches];
+		ourData->endpoint_data_arr[i].caches_latency = new int[ourData->c];
+
+		for (j = 0; j < ourData->c; j++)
+		{
+			cin >> cache_id_temp >> cache_latancy_temp;
+			ourData->endpoint_data_arr[i].caches_latency[j] = 0;
+		}
+
 		for (j = 0; j < ourData->endpoint_data_arr[i].num_of_caches; j++)
 		{
 			cin >> cache_id_temp >> cache_latancy_temp;
@@ -42,6 +49,21 @@ void parser(Data* ourData)
 		}
 
 	}
+	string req_video_temp, req_endpoint_temp, req_size_temp;
+	
+	for (i = 0; i < ourData->r; i++)
+	{
+		cin >> req_video_temp >> req_endpoint_temp >> req_size_temp;
+		Requests* req_temp = new Requests();
+		req_temp->endpoint = stoi(req_endpoint_temp);
+		req_temp->size = stoi(req_size_temp);
+		ourData->video_arr[stoi(req_video_temp)].req.insert(ourData->video_arr[stoi(req_video_temp)].req.begin(), req_temp);
+	}
+}
 
-
+int main()
+{
+	Data* ourData = new Data();
+	parser(ourData);
+	return 0;
 }
--- HashCode/HashCode/parse_input.h
+++ HashCode/HashCode/parse_input.h
@@ -5,18 +5,22 @@
 #include <string>
 #include <fstream>
 #include <iomanip>
+#include <algorithm>
+#include <list>
 
-class Data
-{
+class Requests {
 public:
-	int v, e, r, c, x;
-	int *video_sizes;
-	Endpoint_Data* endpoint_data_arr;
+	int size;
+	int endpoint;
+	Requests(){}
 
-	~Data()
-	{
-		delete[] video_sizes;
-	}
+};
+
+class Video {
+public:
+	int size;
+	std::list<Requests*> req;
+	std::list<Requests*>::iterator req_it;
 };
 
 class Endpoint_Data
@@ -25,6 +29,37 @@ public:
 	int data_center_latency;
 	int num_of_caches;
 	int *caches_latency;
+	Endpoint_Data()
+	{
+		caches_latency = NULL;
+	}
+	~Endpoint_Data()
+	{
+		delete[] caches_latency;
+	}
 };
+
+
+class Data
+{
+public:
+	int v, e, r, c, x;
+	Video *video_arr;
+	Endpoint_Data *endpoint_data_arr;
+	
+	Data()
+	{
+		video_arr = NULL;
+		endpoint_data_arr = NULL;
+	}
+
+
+	~Data()
+	{
+		delete[] video_arr;
+	}
+};
+
+
 
 #endif // !PARSE_INPUT
